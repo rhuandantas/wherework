a49739ba848ea8a2c8be4c46ec6572b026de99f3
Rhuan <rhuannixon@gmail.com>
2020-04-02 23:05:06 -0300

    add cors and static path to file directory

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,9 +1,13 @@
 require('./database/database');
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const routes = require('./routes');
+const path = require('path');
 
+app.use(cors());
 app.use(express.json());
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')))
 app.use(routes);
 
 module.exports = app;
